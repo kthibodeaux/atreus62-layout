591b1cfd0d6e18f4518f2851dfa3eaac94b601a4
Keith Thibodeaux <kthibodeaux@baldwinaviation.com>
2017-04-22 11:14:45 -0400

    Add TMUX layer

--- keymap.c
+++ keymap.c
@@ -2,14 +2,28 @@
 
 #define _DEFAULT 0
 #define _SYMB 1
-#define _NAV 2
-#define _RESET 3
+#define _TMUX 2
+#define _NAV 3
+#define _RESET 4
+
+#define M_TMUX_1 M(1)
+#define M_TMUX_2 M(2)
+#define M_TMUX_3 M(3)
+#define M_TMUX_4 M(4)
+#define M_TMUX_5 M(5)
+#define M_TMUX_COPY_MODE M(6)
+#define M_TMUX_SP M(7)
+#define M_TMUX_VS M(8)
+#define M_TMUX_NEW M(9)
+#define M_TMUX_ZOOM M(10)
+#define M_TMUX_SHOW_CURRENT_STORY M(11)
 
 enum {
   TD_SPACE
 };
 
 #define NO_KEY KC_NO
+#define TERM_PASTE LALT(LSFT(KC_INS))
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_DEFAULT] = {
@@ -26,6 +40,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   { KC_TRNS,  KC_AMPR, KC_PIPE, KC_QUOT, KC_DQUO, KC_TRNS, NO_KEY,  KC_VOLU, KC_COLN, KC_AT,   KC_MINS, KC_HASH, KC_TRNS },
   { KC_TRNS,  KC_TRNS, KC_TRNS, KC_PLUS, KC_EQL,  KC_TRNS, KC_TRNS,  KC_VOLD, KC_LBRC, KC_RBRC, KC_TRNS, KC_TRNS, KC_TRNS },
   { KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS }
+},
+
+[_TMUX] = {
+  { KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,    KC_TRNS,  NO_KEY,       KC_TRNS, KC_TRNS,                   KC_TRNS, KC_TRNS,   KC_TRNS,   KC_TRNS },
+  { KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,    KC_TRNS,  NO_KEY,       KC_TRNS, KC_TRNS,                   KC_TRNS, KC_TRNS,   KC_TRNS,   KC_TRNS },
+  { KC_TRNS,  M_TMUX_1, M_TMUX_2, M_TMUX_3, M_TMUX_4,   M_TMUX_5, NO_KEY,       KC_TRNS, M_TMUX_COPY_MODE,          KC_TRNS, M_TMUX_VS, M_TMUX_SP, KC_TRNS },
+  { KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  TERM_PASTE, KC_TRNS,  M_TMUX_NEW,   KC_TRNS, M_TMUX_SHOW_CURRENT_STORY, KC_TRNS, KC_TRNS,   KC_TRNS,   KC_TRNS },
+  { KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,    KC_TRNS,  M_TMUX_ZOOM,  KC_TRNS, KC_TRNS,                   KC_TRNS, KC_TRNS,   KC_TRNS,   KC_TRNS }
 },
 
 [_NAV] = {
@@ -65,6 +87,19 @@ qk_tap_dance_action_t tap_dance_actions[] = {
   [TD_SPACE] = ACTION_TAP_DANCE_DOUBLE(KC_SPC, KC_UNDERSCORE)
 };
 
+void do_tmux_key(keyrecord_t *record, uint8_t code, uint8_t modifier) {
+  if (record->event.pressed) {
+    register_code(KC_LCTRL);
+    register_code(KC_SPC);
+    unregister_code(KC_SPC);
+    unregister_code(KC_LCTRL);
+    register_code(modifier);
+    register_code(code);
+    unregister_code(code);
+    unregister_code(modifier);
+  }
+}
+
 const uint16_t PROGMEM fn_actions[] = { };
 
 const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt)
@@ -78,6 +113,39 @@ const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt)
     else {
       unregister_code(KC_RSFT);
     }
+    break;
+  case 1:
+    do_tmux_key(record, KC_1, KC_NO);
+    break;
+  case 2:
+    do_tmux_key(record, KC_2, KC_NO);
+    break;
+  case 3:
+    do_tmux_key(record, KC_3, KC_NO);
+    break;
+  case 4:
+    do_tmux_key(record, KC_4, KC_NO);
+    break;
+  case 5:
+    do_tmux_key(record, KC_5, KC_NO);
+    break;
+  case 6:
+    do_tmux_key(record, KC_LBRC, KC_NO);
+    break;
+  case 7:
+    do_tmux_key(record, KC_QUOT, KC_LSFT);
+    break;
+  case 8:
+    do_tmux_key(record, KC_5, KC_LSFT);
+    break;
+  case 9:
+    do_tmux_key(record, KC_C, KC_NO);
+    break;
+  case 10:
+    do_tmux_key(record, KC_Z, KC_NO);
+    break;
+  case 11:
+    do_tmux_key(record, KC_J, KC_NO);
     break;
   }
   return MACRO_NONE;
