d34d36b552e9fadc5fb31c2c0bfff87f5d4082ce
Keith Thibodeaux <kthibodeaux@baldwinaviation.com>
2017-04-22 10:03:18 -0400

    Space tap dances to underscore

--- keymap.c
+++ keymap.c
@@ -4,13 +4,17 @@
 #define _NAV 1
 #define _RESET 2
 
+enum {
+  TD_SPACE
+};
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_DEFAULT] = {
   { KC_ESC,   KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_TRNS,  KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS },
   { KC_BSLS,  KC_Q,    KC_W,    KC_F,    KC_P,    KC_G,    KC_TRNS,  KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN,    KC_RBRC },
   { KC_TAB,   KC_A,    ALT_T(KC_R),    KC_S,    CTL_T(KC_T),    KC_D,    KC_TRNS,  KC_H,    CTL_T(KC_N),    KC_E,    ALT_T(KC_I),    KC_O, KC_QUOT },
   { KC_LSFT,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_DELT,  KC_K,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_LBRC },
-  { KC_LCTL,  KC_LGUI, KC_LALT, KC_GRV,  MO(_NAV),KC_BSPC, KC_ENT,   KC_SPC,  KC_EQL,  KC_MINS, KC_QUOT, KC_ENT,  KC_RGUI }
+  { KC_LCTL,  KC_LGUI, KC_LALT, KC_GRV,  MO(_NAV),KC_BSPC, KC_ENT,   TD(TD_SPACE),  KC_EQL,  KC_MINS, KC_QUOT, KC_ENT,  KC_RGUI }
 },
 
 [_NAV] = {
@@ -46,7 +50,9 @@ void do_tap_dance (qk_tap_dance_state_t *state) {
   }
 }
 
-qk_tap_dance_action_t tap_dance_actions[] = { };
+qk_tap_dance_action_t tap_dance_actions[] = {
+  [TD_SPACE] = ACTION_TAP_DANCE_DOUBLE(KC_SPC, KC_UNDERSCORE)
+};
 
 const uint16_t PROGMEM fn_actions[] = { };
 
