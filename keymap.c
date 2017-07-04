24d3e8d553544aad1607633f07bbeb8e7d16286a
Keith Thibodeaux <keith@railscoder.net>
2017-07-04 11:20:42 -0400

    Add numpad layer

--- keymap.c
+++ keymap.c
@@ -5,14 +5,15 @@
 enum {
   _DEFAULT,
   _SYMB,
-  _TMUX
+  _TMUX,
+  _NUM
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_DEFAULT] = {
   { KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,   NO_KEY,  KC_6,   KC_7,      KC_8,    KC_9,    KC_0,    KC_TRNS },
   { KC_DELT, KC_Q,    KC_W,    KC_F,    KC_P,    KC_G,   NO_KEY,  KC_J,   KC_L,      KC_U,    KC_Y,    KC_SCLN, KC_BSLS },
-  { KC_TAB,  KC_A,    MY_R,    KC_S,    MY_T,    KC_D,   NO_KEY,  KC_H,   MY_N,      MY_E,    MY_I,    KC_O,    KC_TRNS },
+  { KC_TAB,  KC_A,    MY_R,    MY_S,    MY_T,    KC_D,   NO_KEY,  KC_H,   MY_N,      MY_E,    MY_I,    KC_O,    KC_TRNS },
   { KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,   KC_BSPC, KC_K,   KC_M,      KC_COMM, KC_DOT,  KC_SLSH, KC_LSFT },
   { KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, MY_SPC, KC_TRNS, KC_ENT, MO(_SYMB), KC_LEFT, KC_UP,   KC_DOWN, KC_RIGHT }
 },
@@ -31,7 +32,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   { KC_TRNS, M(M_TMUX_1), M(M_TMUX_2), M(M_TMUX_3),        M(M_TMUX_4),      M(M_TMUX_5),          NO_KEY,         KC_TRNS, M(M_TMUX_COPY_MODE),          KC_TRNS, M(M_TMUX_VS), M(M_TMUX_SP), KC_TRNS },
   { KC_TRNS, KC_TRNS,     KC_TRNS,     M(M_TMUX_OPEN_URL), TERM_PASTE,       M(M_TMUX_BREAK_PANE), M(M_TMUX_NEW),  KC_TRNS, M(M_TMUX_SHOW_CURRENT_STORY), KC_TRNS, KC_TRNS,      KC_TRNS,      KC_TRNS },
   { KC_TRNS, KC_TRNS,     KC_TRNS,     KC_TRNS,            KC_TRNS,          KC_TRNS,              M(M_TMUX_ZOOM), KC_TRNS, KC_TRNS,                      KC_TRNS, KC_TRNS,      KC_TRNS,      KC_TRNS }
-}
+},
+
+[_NUM] = {
+  { KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, NO_KEY,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS },
+  { KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, NO_KEY,  KC_7,    KC_8,    KC_9, KC_TRNS, KC_TRNS, KC_TRNS },
+  { KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, NO_KEY,  KC_4,    KC_5,    KC_6, KC_TRNS, KC_TRNS, KC_TRNS },
+  { KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_1,    KC_2,    KC_3, KC_TRNS, KC_TRNS, KC_TRNS },
+  { KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_0,    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS }
+},
 
 /*
 [_TRNS] = {
